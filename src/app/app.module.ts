b879509844b40140aaed6c6acaeb2cf61830e6bc
Yve Carelle <nguetcheyvecarelle@gmail.com>
2019-12-04 17:49:26 +0100

    use mdb input search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { InputsModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    InputsModule,
+    WavesModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
