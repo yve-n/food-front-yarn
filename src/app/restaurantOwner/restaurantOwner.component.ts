42f95f93d70267c4b051b68cafedbd32097b30e4
Patrick Akim KABINDA <akimkabinda@skylink.cd>
2019-12-26 14:22:14 +0100

    Rename component uppercase to lowercase

---
+++ src/app/restaurantOwner/restaurantOwner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantOwner',
+  templateUrl: './restaurantOwner.component.html',
+  styleUrls: ['./restaurantOwner.component.scss']
+})
+export class RestaurantOwnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
