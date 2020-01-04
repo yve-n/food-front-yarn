5a6372fc6de5b85d0e615c40b06b376ec45e33ad
Yve Carelle <nguetcheyvecarelle@gmail.com>
2020-01-04 19:13:01 +0100

    Add restaurantOwnerForm.

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -2,7 +2,7 @@ export abstract class Account {
   id: number;
   firstName: string;
   lastName: string;
-  phoneNumber?: string;
+  number?: string;
   email: string;
   password: string;
 }
--- src/app/restaurantOwner/restaurantOwner.component.ts
+++ src/app/restaurantOwner/restaurantOwner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-
+import { RestaurantOwner } from "../models/restaurantOwner";
 @Component({
   selector: "app-restaurantOwner",
   templateUrl: "./restaurantOwner.component.html",
@@ -13,21 +13,58 @@ export class RestaurantOwnerComponent implements OnInit {
 
   ngOnInit() {
     this.restaurantForm = this.fb.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required],
-      email: ["", Validators.required],
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      lastName: ["", [Validators.required, Validators.minLength(3)]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]],
       number: ["", Validators.required],
-      restaurantName: ["", Validators.required],
+      restaurantName: ["", [Validators.required, Validators.minLength(3)]],
       address: ["", Validators.required],
-      town: ["", Validators.required],
+      town: ["", [Validators.required, Validators.minLength(3)]],
       postalCode: ["", Validators.required],
       specialities: ["", Validators.required],
       delivery: ["", Validators.required],
       livreurs: ["", Validators.required]
     });
   }
+  get email() {
+    return this.restaurantForm.get("email");
+  }
+  get password() {
+    return this.restaurantForm.get("password");
+  }
+  get lastName() {
+    return this.restaurantForm.get("lastName");
+  }
+  get firstName() {
+    return this.restaurantForm.get("firstName");
+  }
+  get number() {
+    return this.restaurantForm.get("number");
+  }
+  get address() {
+    return this.restaurantForm.get("address");
+  }
+  get town() {
+    return this.restaurantForm.get("town");
+  }
+  get restaurantName() {
+    return this.restaurantForm.get("restaurantName");
+  }
+  get specialities() {
+    return this.restaurantForm.get("specialities");
+  }
+  get postalCode() {
+    return this.restaurantForm.get("postalCode");
+  }
+  get delivery() {
+    return this.restaurantForm.get("delivery");
+  }
+  get livreurs() {
+    return this.restaurantForm.get("livreurs");
+  }
 
-  onSubmit({ value, valid }: { value: Validators; valid: boolean }) {
+  onSubmit({ value, valid }: { value: RestaurantOwner; valid: boolean }) {
     console.log(value, valid);
   }
 }
